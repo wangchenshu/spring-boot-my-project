57a8ed10f55425916f85fd9c71b6c64916d29928
Chen Shu <csw.dawn@gmail.com>
2019-12-04 22:18:44 +0800

    add sql file

---
+++ my_project.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2019 年 12 月 01 日 19:55
+-- 伺服器版本： 10.3.20-MariaDB-0ubuntu0.19.10.1
+-- PHP 版本： 7.3.12-1+ubuntu19.10.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `my_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `carts`
+--
+
+CREATE TABLE `carts` (
+  `id` int(11) NOT NULL,
+  `messenger_user_id` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `fb_users`
+--
+
+CREATE TABLE `fb_users` (
+  `id` int(11) NOT NULL,
+  `messenger_user_id` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `profile_pic_url` text NOT NULL,
+  `timezone` int(11) NOT NULL,
+  `locale` varchar(10) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `messenger_user_id` varchar(100) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `detail` text NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_type_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(10) UNSIGNED NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `products`
+--
+
+INSERT INTO `products` (`id`, `product_type_id`, `name`, `price`, `description`, `created_at`, `updated_at`) VALUES
+(1, 1, 'product-1', 10, 'this is product 1', '2019-11-23 18:17:02', '2019-11-23 19:22:14'),
+(2, 2, 'product-2', 20, 'this is product 2', '2019-11-23 18:25:05', '2019-11-23 19:22:21');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product_type`
+--
+
+CREATE TABLE `product_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `product_type`
+--
+
+INSERT INTO `product_type` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'type1', 'this is type1', '2019-11-23 17:34:12', '2019-11-23 19:23:36'),
+(2, 'type2', 'this is type2', '2019-11-23 17:34:27', '2019-11-23 19:23:44');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'test1', '$2a$14$Jrq5TzhoSBC1V/1GDFFkCemNS5wW7XqMOB6MpohI4HdDCTR0gw696', '2019-11-23 19:03:48', '2019-12-01 19:53:00'),
+(2, 'test2', '$2a$14$sxT82K3u9ynGCO6WWnqUdO6gCA5GnYYwgSfKq3mVX2.Oz9nA7NPuy', '2019-11-23 19:03:56', '2019-12-01 19:53:28'),
+(3, 'test3', '$2a$14$Pyfk.cMP9h8kiZtSTl595ekkLyXO2FcNAaGfsVB3ze0n6s0DAFqB.', '2019-11-23 19:51:28', '2019-12-01 19:53:43');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `fb_users`
+--
+ALTER TABLE `fb_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `messenger_user_id` (`messenger_user_id`);
+
+--
+-- 資料表索引 `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `product_type_id` (`product_type_id`);
+
+--
+-- 資料表索引 `product_type`
+--
+ALTER TABLE `product_type`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `fb_users`
+--
+ALTER TABLE `fb_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `product_type`
+--
+ALTER TABLE `product_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
